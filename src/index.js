648d737e120ab4eeec27adcff954b433fecef292
Subash Basnet <subashbasnet@Subashs-MacBook-Pro-2.local>
2020-11-01 13:22:24 +0200

    Replacing useState with useReducer

--- src/index.js
+++ src/index.js
@@ -1,37 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function GitHubUser({login}){
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${login}`)
-        .then(res => res.json()) // converting the response to json format.
-        .then(setData)
-        .catch(console.error);
-    });
+function Checkbox(){
+    const [checked, toggle] = useReducer(
+        checked => !checked,
+        false
+    );
     
-    if(data){
-        return (
-            <div> 
-            <h2>{JSON.stringify(data)}</h2>
-        
-            <h1>{data.login}</h1>
-            <img src={data.avatar_url} width={100} />
-            </div>
-        );
-            
-    }
-    return null;
-}
-
-function App(){
-    return <GitHubUser login= "subasnet" />
+    return(
+        <>
+            <input
+                type = "checkbox"
+                value = {checked}
+                onChange = {toggle}
+            />
+            {checked ? "checked": "not checked"}
+        < />
+    );
 }
 
 ReactDOM.render(
-    <App />,
+    <Checkbox />,
   document.getElementById('root')
 );
 
