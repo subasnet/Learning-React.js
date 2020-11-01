124992164dbfabc954f1d6dbac97be9fdc77862e
Subash Basnet <subashbasnet@Subashs-MacBook-Pro-2.local>
2020-11-01 12:55:54 +0200

    Fetching data with useEffect from GitHub API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-ReactDOM.render(React.createElement("h1", { style: { color: "blue" } }, "Hello!"),
+function GitHubUser({login}){
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${login}`)
+        .then(res => res.json()) // converting the response to json format.
+        .then(setData)
+        .catch(console.error);
+    });
+    
+    if(data){
+        return (
+            <div> 
+            <h2>{JSON.stringify(data)}</h2>
+        
+            <h1>{data.login}</h1>
+            <img src={data.avatar_url} width={100} />
+            </div>
+        );
+            
+    }
+    return null;
+}
+
+function App(){
+    return <GitHubUser login= "subasnet" />
+}
+
+ReactDOM.render(
+    <App />,
   document.getElementById('root')
 );
+
